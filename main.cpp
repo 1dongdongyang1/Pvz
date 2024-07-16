80c5541973cf0a4b3223011675784f1722c9c01c
Zhang Jiahui <shuaigejiahui@163.com>
2024-07-16 17:47:55 +0800

    植物的选择和拖动

--- main.cpp
+++ main.cpp
@@ -13,34 +13,60 @@ enum
 };
 
 IMAGE imgBg;	
-IMAGE imgBar;	//植物卡牌栏
+IMAGE imgBar;
 IMAGE imgCards[plantCount];
+IMAGE* imgPlant[plantCount][20];
+
+int curX, curY;	//当前选中的植物，在移动过程中的位置
+int curPlant;	//0：没有选中  1：选择第一种植物
+
+bool fileExist(const char* name)	//判断文件是否存在
+{
+	FILE* fp = fopen(name,"r");		//若文件为空则返回NULL
+	if (fp)
+	{
+		fclose(fp);
+	}
+	return fp != NULL;
+}
 
 void gameInit()
 { 
 	loadimage(&imgBg, "res/bg.jpg");
-	loadimage(&imgBar, "res/bar5.png");	//初始化卡牌栏
+	loadimage(&imgBar, "res/bar5.png");
 
-	//初始化植物卡牌
+	memset(imgPlant, 0, sizeof(imgPlant));
+
 	char name[64];
 	for (int i = 0; i < plantCount; i++)
 	{
-		//生成植物卡牌的文件名
-		sprintf_s(name, sizeof(name), "res/Cards/card_%d.png", i + 1);	//注意这里使用的是vs编译器自带的sprintf_s
+		sprintf_s(name, sizeof(name), "res/Cards/card_%d.png", i + 1);
 		loadimage(&imgCards[i], name);
-	}
-	
-	initgraph(WIN_WIDTH, WIN_HEIGHT);
 
+		//生成植物每帧的图片
+		for (int j = 0; j < 20; j++)
+		{
+			sprintf_s(name, sizeof(name), "res/zhiwu/%d/%d.png", i, j + 1);
+			//先判断这个文件是否存在
+			if (fileExist(name))
+			{
+				imgPlant[i][j] = new IMAGE;	//C++
+				loadimage(imgPlant[i][j], name);
+			}
+			else
+				break;
+		}
+	}
+
+	curPlant = 0;
+	initgraph(WIN_WIDTH, WIN_HEIGHT);
 }
 
 void updateWindow()
 {
+	BeginBatchDraw();	//开始缓冲
 	putimage(0, 0, &imgBg);
-	//putimage(250, 0, &imgBar);
-	putimagePNG(250, 0, &imgBar);	//渲染卡牌栏
-
-	//渲染卡牌
+	putimagePNG(250, 0, &imgBar);
 	for (int i = 0; i < plantCount; i++)
 	{
 		int x = 338 + i * 65;
@@ -48,13 +74,51 @@ void updateWindow()
 		putimagePNG(x, y, &imgCards[i]);
 	}
 
+	//渲染拖动过程中的植物
+	if (curPlant)
+	{
+		IMAGE* img = imgPlant[curPlant - 1][0];
+		putimagePNG(curX - img->getwidth() / 2, curY - img->getheight() / 2, img);	//这里用到了C++的类
+	}
+
+	EndBatchDraw();	//结束双缓冲
+}
+
+void userClick()
+{
+	ExMessage msg;
+	static int status = 0;
+	if (peekmessage(&msg))
+	{
+		if (msg.message == WM_LBUTTONDOWN)
+		{
+			if (msg.x > 338 && msg.x < 338 + 65 * plantCount && msg.y < 96)
+			{
+				int index = (msg.x - 338) / 65;
+				status = 1;
+				curPlant = index + 1;
+			}
+		}
+		else if (msg.message == WM_MOUSEMOVE && status == 1)
+		{
+			curX = msg.x;
+			curY = msg.y; 
+		}
+		else if (msg.message == WM_LBUTTONUP)
+		{
+
+		}
+	}
 }
 
 int main(void)
 {
 	gameInit();
-	updateWindow();
+	while (1)
+	{
+		userClick();
+		updateWindow();
 
-	system("pause");
+	}
 	return 0;
 }
